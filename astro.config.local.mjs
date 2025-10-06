954fe7478ca2c976fe2795c296a88b6df51f7237
Maximiliano <maximiliano.furtado@besistemas.com.br>
2025-10-05 23:33:23 -0300

    fix deploy

--- astro.config.local.mjs
+++ astro.config.local.mjs
@@ -5,8 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://maax103.github.io',
-  base: '/studioline',
   output: 'static',
   vite: {
     ssr: {
