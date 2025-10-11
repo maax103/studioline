2ff478074240828040461e7616ae70a53bb1e21b
Maximiliano <maximiliano.furtado@besistemas.com.br>
2025-10-11 17:37:09 -0300

    fix embed param

--- src/react/pages/ProjectDetailsWrapper.tsx
+++ src/react/pages/ProjectDetailsWrapper.tsx
@@ -3,16 +3,26 @@ import { theme } from "../../theme";
 import { ProjectsProvider } from "../contexts/ProjectsContext";
 import { ProjectDetailsPage } from "./ProjectDetailsPage";
 import { Footer } from "../components/layout";
+import { useEffect, useState } from "react";
 
 export const ProjectDetailsWrapper = ({
   id,
   currentPath,
-  isEmbed,
 }: {
   id: string;
   currentPath: string;
-  isEmbed: boolean;
 }) => {
+  const [isEmbed, setIsEmbed] = useState(false);
+
+  useEffect(() => {
+    // Check for embed parameter on the client side
+    if (typeof window !== "undefined") {
+      const urlParams = new URLSearchParams(window.location.search);
+      const embedParam = urlParams.get("embed");
+      setIsEmbed(embedParam !== null && embedParam !== "false");
+    }
+  }, []);
+
   return (
     <MantineProvider theme={theme}>
       <ProjectsProvider>
